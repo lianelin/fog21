17dde18d0ca84cb9d6431f90f3fad93057f211d0
lianelin <53479715+lianelin@users.noreply.github.com>
2021-06-24 15:43:44 +0200

    Update BrightnessSensor.java

--- Edge/src/BrightnessSensor.java
+++ Edge/src/BrightnessSensor.java
@@ -4,7 +4,7 @@ import java.util.Random;
 public class BrightnessSensor extends Sensor {
 	Random r = new java.util.Random();
 	
-	public double getData()
+	public String getData()
 	{
 		LocalTime time = LocalTime.now();
 		double timedist = time.getMinute() + time.getHour()*60; //linear time curve from 0 to 24h
@@ -14,11 +14,16 @@ public class BrightnessSensor extends Sensor {
 		}
 		timedist = timedist / 720 + r.nextGaussian()*0.01; //normalize and add random noise
 		
-		return timedist;
+		String value = String.valueOf(timedist);
+        	return value;
 	}
 	
 	public dataSource getType()
 	{
 		return dataSource.BRIGHTNESS;
 	}
+	
+	public String toString() {
+        return "BRIGHTNESS";
+    }
 }
