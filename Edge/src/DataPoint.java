5a1fc5162e5aac54be13989cadb17f0790efa9fd
lianelin <53479715+lianelin@users.noreply.github.com>
2021-06-24 02:10:26 +0200

    Update DataPoint.java

--- Edge/src/DataPoint.java
+++ Edge/src/DataPoint.java
@@ -4,20 +4,25 @@ public class DataPoint {
 	public dataSource source;
 	public double value;
 	public double time;
+	public int ID;
+        public static final int EDGE_ID = 1;
 	
 	public DataPoint(dataSource source, double value, double time)
 	{
 		this.source = source;
 		this.value = value;
 		this.time = time;
+		this.ID = (int) Math.floor(Math.random()*65535);
 	}
 	
 	public JSONObject getJSON()
 	{
 		JSONObject data = new JSONObject();
+		data.put("EDGE_ID", EDGE_ID);
 		data.put("timestamp", this.time);
 		data.put("type", this.source);
 		data.put("value", this.value);
+		data.put("ID", this.ID);
 		return data;
 	}
 	
