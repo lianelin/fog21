cf36da2f8976e1bbb7e5cdb41a7512a68743b720
lianelin <53479715+lianelin@users.noreply.github.com>
2021-06-24 02:16:37 +0200

    Update TemperatureSensor.java

--- Edge/src/TemperatureSensor.java
+++ Edge/src/TemperatureSensor.java
@@ -6,14 +6,19 @@ public class TemperatureSensor extends Sensor {
 	double lastValue = center;
 	Random r = new Random();
 	
-	public double getData()
+	public String getData()
 	{
-		lastValue = lastValue + r.nextGaussian() * (0.001 + 1/(Utils.clamp(Math.abs(lastValue - center), 10, 1000))); //add a random gauss value between 0.001 and 0.1 to the last value to simulate temperature changes. The value is centered around 22°.
-		return lastValue;
+		lastValue = lastValue + r.nextGaussian() * (0.001 + 1/(Utils.clamp(Math.abs(lastValue - center), 10, 1000))); //add a random gauss value between 0.001 and 0.1 to the last value to simulate temperature changes. The value is centered around 22Â°.
+		String value = String.valueOf(lastValue);
+                return value;
 	}
 	
 	public dataSource getType()
 	{
 		return dataSource.TEMPERATURE;
 	}
+	
+	public String toString(){
+            return "TEMPERATURE";
+        }
 }
